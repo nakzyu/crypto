dc7f6987f1b204e2dce6f29e9b9e4fa26631da27
nakzyu <59201980+nakzyu@users.noreply.github.com>
2020-05-05 20:42:24 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+
+const d = () => {
+  var xhr = new XMLHttpRequest();
+  var url =
+    "http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo"; /*URL*/
+  var queryParams =
+    "?" +
+    encodeURIComponent("ServiceKey") +
+    "=" +
+    encodeURIComponent(
+      "7wZBl12jJ2YDrzKT03mkhLh7ioxHTglyo7EvgWZAuBv2pPBzApWgdbIzC9ynzuCknTWpemNbioarZ6rYzMGVEw=="
+    ); /*Service Key*/
+  queryParams +=
+    "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /**/
+  queryParams +=
+    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+  queryParams +=
+    "&" +
+    encodeURIComponent("depPlaceId") +
+    "=" +
+    encodeURIComponent("NAT010000"); /**/
+  queryParams +=
+    "&" +
+    encodeURIComponent("arrPlaceId") +
+    "=" +
+    encodeURIComponent("NAT011668"); /**/
+  queryParams +=
+    "&" +
+    encodeURIComponent("depPlandTime") +
+    "=" +
+    encodeURIComponent("20200101"); /**/
+  queryParams +=
+    "&" +
+    encodeURIComponent("trainGradeCode") +
+    "=" +
+    encodeURIComponent("00"); /**/
+  xhr.open("GET", url + queryParams);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      alert(
+        "Status: " +
+          this.status +
+          "nHeaders: " +
+          JSON.stringify(this.getAllResponseHeaders()) +
+          "nBody: " +
+          this.responseText
+      );
+    }
+  };
+
+  xhr.send("");
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Dsd
+      <button onClick={() => console.log(d())}></button>
     </div>
   );
 }
