cf4bbe2db0e7d8ae31e7306acea07d2de4e49ae7
nakzyu <59201980+nakzyu@users.noreply.github.com>
2020-05-18 01:34:01 +0900

    add container

--- src/components/CoinRank.js
+++ src/components/CoinRank.js
@@ -23,75 +23,79 @@ const CoinRank = () => {
   useEffect(() => fetchMore(), []);
 
   return (
-    <div className="is-flex direction-column table-container">
-      <table className="table  table is-bordered is-hoverable is-fullwidth">
-        <thead>
-          <tr>
-            <th className="is-narrow">Rank</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Market Cap</th>
-            <th>Volume (24Hr)</th>
-            <th>Change (24Hr)</th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <th>Rank</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Market Cap</th>
-            <th>Volume (24Hr)</th>
-            <th>Change (24Hr)</th>
-          </tr>
-        </tfoot>
-        <tbody>
-          {coin &&
-            coin.map((k) => (
-              <tr
-                className="anchor"
-                key={`${k.rank}`}
-                id={`${k.rank}`}
-                name={`${k.rank}`}
-              >
-                <td>{k.rank}</td>
-                <td
-                  className={`is-flex  has-text-white ${handleBackgroundColor(
-                    k.changePercent24Hr
-                  )}`}
-                >
-                  <figure className="image is-32x32">
-                    <img
-                      src={`https://static.coincap.io/assets/icons/${k.symbol.toLowerCase()}@2x.png`}
-                      onError={(e) => {
-                        e.target.src = require("../assets/logo_mark.png");
-                      }}
-                      alt={k.name}
-                    />
-                  </figure>
-                  <div>{k.name}</div>
-                </td>
-                <td>${handleDollar(k.priceUsd)}</td>
-                <td>${handleBigNum(k.marketCapUsd)}</td>
-                <td>${handleBigNum(k.volumeUsd24Hr)}</td>
-                <td className={`${handleTextColor(k.changePercent24Hr)}`}>
-                  {k.changePercent24Hr > 0 ? "+" : null}
-                  {handlePercent(k.changePercent24Hr)}%
-                </td>
+    <div className="container">
+      <div className="notification">
+        <div className="is-flex direction-column table-container">
+          <table className="table  table is-bordered is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th className="is-narrow">Rank</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Market Cap</th>
+                <th>Volume (24Hr)</th>
+                <th>Change (24Hr)</th>
               </tr>
-            ))}
-        </tbody>
-      </table>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Market Cap</th>
+                <th>Volume (24Hr)</th>
+                <th>Change (24Hr)</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              {coin &&
+                coin.map((k) => (
+                  <tr
+                    className="anchor"
+                    key={`${k.rank}`}
+                    id={`${k.rank}`}
+                    name={`${k.rank}`}
+                  >
+                    <td>{k.rank}</td>
+                    <td
+                      className={`is-flex  has-text-white ${handleBackgroundColor(
+                        k.changePercent24Hr
+                      )}`}
+                    >
+                      <figure className="image is-32x32">
+                        <img
+                          src={`https://static.coincap.io/assets/icons/${k.symbol.toLowerCase()}@2x.png`}
+                          onError={(e) => {
+                            e.target.src = require("../assets/logo_mark.png");
+                          }}
+                          alt={k.name}
+                        />
+                      </figure>
+                      <div>{k.name}</div>
+                    </td>
+                    <td>${handleDollar(k.priceUsd)}</td>
+                    <td>${handleBigNum(k.marketCapUsd)}</td>
+                    <td>${handleBigNum(k.volumeUsd24Hr)}</td>
+                    <td className={`${handleTextColor(k.changePercent24Hr)}`}>
+                      {k.changePercent24Hr > 0 ? "+" : null}
+                      {handlePercent(k.changePercent24Hr)}%
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
 
-      <a
-        className="button is-success is-rounded"
-        href={`#${fetchLimit - 29}`}
-        onClick={() => {
-          fetchMore();
-        }}
-      >
-        fetch more
-      </a>
+          <a
+            className="button is-success is-rounded"
+            href={`#${fetchLimit - 29}`}
+            onClick={() => {
+              fetchMore();
+            }}
+          >
+            fetch more
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
